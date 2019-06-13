733ba3ee74f884a5e2dc84b2301a268466ce9557
Yilin Lee <asd840408@gmail.com>
2019-06-13 22:55:24 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import List from '@/components/List'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/hello',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'List',
+      component: List
+    }
+  ],
+  mode: 'history',
+})
